683178a0b50ac4ea04b3bfea657e41f495079a27
djack2017 <djack@djack.com.pl>
2026-02-11 12:55:57 +0100

    Update z2s_web_gui.cpp

--- examples/Z2S_Gateway/z2s_web_gui.cpp
+++ examples/Z2S_Gateway/z2s_web_gui.cpp
@@ -1231,12 +1231,9 @@ void buildCredentialsZabbix() {
 		(void*)GUI_CB_SAVE_FLAG);
 
 	//===========================================================
-	auto myCfg = Supla::Storage::ConfigInstance();
   
 	if (myCfg) {
 		memset(general_purpose_gui_buffer, 0, sizeof(general_purpose_gui_buffer));
-//		if (myCfg->getZabbixServer(general_purpose_gui_buffer) && strlen(general_purpose_gui_buffer) > 0)
-//			ESPUI.updateText(zabbix_server_text, general_purpose_gui_buffer);
 		if (myCfg.getZabbixServer(general_purpose_gui_buffer) && strlen(general_purpose_gui_buffer) > 0) {
 			ESPUI.updateText(zabbix_server_text, general_purpose_gui_buffer);
 		}	
